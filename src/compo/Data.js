457db13dbe6c513046c3da45757584c3c17c3ff5
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-16 22:41:02 -0600

    update

---
+++ src/compo/Data.js
@@ -0,0 +1,67 @@
+ const  data = [
+  {
+    "createdAt": "2021-11-01T11:44:12.730Z",
+    "id": "2",
+    "image": "https://appjwla9e860a2252f947e3b518387ea762e7e6113949-dev.s3.amazonaws.com/public/images/Agollas.jpeg",
+    "name": "Argollas",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:44:12.730Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:53:18.658Z",
+    "id": "8",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/Pulsos.jpeg",
+    "name": "Pulsos",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:53:18.658Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:44:12.730Z",
+    "id": "9",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/gargantilla-con-sol.jpeg",
+    "name": "Esclavas",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:44:12.730Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:42:19.781Z",
+    "id": "1",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/aretes-aros-perlas.jpeg",
+    "name": "Conjuntos",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:42:19.781Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:48:51.783Z",
+    "id": "5",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/manillasjpeg.jpeg",
+    "name": "Manillas",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:48:51.783Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:47:13.255Z",
+    "id": "4",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/cadena-con-dije-2.jpeg",
+    "name": "Cadenas",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:47:13.255Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:51:37.151Z",
+    "id": "7",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/gargantilla-con-dije-3.jpeg",
+    "name": "Gargantillas",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:51:37.151Z"
+  },
+  {
+    "createdAt": "2021-11-01T11:45:30.043Z",
+    "id": "3",
+    "image": "https://ecomercejew7b6e695de0ef4126badbc7db32fd927c112134-dev.s3.amazonaws.com/public/Aro-con-oso.jpeg",
+    "name": "Aritos",
+    "price": 1.5,
+    "updatedAt": "2021-11-01T11:45:30.043Z"
+  }
+]
+export default data
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,66 @@
 import React from 'react'
 import style from 'styled-components'
 import {mobilex,mobile,all,table} from '../compo/Responsive'
+import Data from '../compo/Data'
+import data from '../compo/Data';
 
 const Container = style.div`
-
 display:flex;
-justify-content:center;
-flex-direction:row;
-height:60vh;
-box-size:border-box;
-width:90%;
 ${mobile({
   flexDirection: 'column',
-  height:"300vh",
-  flexWrapper:'wrapper'
 })}
 `
-const Item = style.div`
-border:1px solid black;
-width:27%;
-border-radius:20px 20px;
-margin:10px;
-display:flex;
-justify-content:center;
-align-items:center;
+const ItemContainer = style.div`
+
+width:44%;
+padding:20px;
+margin:20px;
+text-align:center;
 ${mobile({
   width:'100%',
-  height:'35vh',
-  padding:'10px'
+  flexDirection:'column',
+  justifyContent:'center',
+  alignItems: 'center',
+  margin: '3px',
+  padding: '0',
+  border:'1px solid black'
+  
+ 
+ 
 })}
+`
+const Image = style.img`
+width:100%;
+border-radius:5px 10px;
+${mobile({
+  width:'50%',
+  height:'40vh',
+  
+})}
+`
+const InfoContainer = style.div`
+margin:10px;
+`
+const Info = style.section`
+
 `
 
 const Home = () => {
   return (
     <Container>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
-      <Item>1</Item>
+      {data.map((item ,i )=>{
+  return (<>
+  <ItemContainer>
+        <Image src= {item.image} alt="picture"/>
+        <InfoContainer>
+        <Info>
+          {item.name}
+        </Info>
+      </InfoContainer>
+      </ItemContainer>
+     
+  </>)
+      })}
      
     </Container>
   )
