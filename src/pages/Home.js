6dc4a1de8b33a19fc2381da871f9f7981d463790
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-16 23:51:07 -0600

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,11 @@ ${mobile({
   flexDirection: 'column',
 })}
 `
+const H1 = style.h1`
+width:100%;
+font-size:50px;
+text-align:center;
+`
 const ItemContainer = style.div`
 display:flex;
 justify-content:center;
@@ -36,6 +41,7 @@ const Image = style.img`
 width:90%;
 height:40vh;
 border-radius:5px 10px;
+object-fit:cover;
 ${mobile({
   width:'88%',
   height:'48vh',
@@ -46,14 +52,16 @@ const InfoContainer = style.div`
 margin:10px;
 `
 const Info = style.section`
-
+font-size:30px;
 `
 
 const Home = () => {
   return (
     <Container>
+      <H1>Categorias</H1>
       {data.map((item ,i )=>{
   return (<>
+  
   <ItemContainer>
         <Image src= {item.image} alt="picture"/>
         <InfoContainer>
